e51b736e2220bd1403f513c6b866d2e5ba8ac680
aneeshzed <aneesh2405@gmail.com>
2023-01-12 12:57:22 +0530

    Added new function
    added new function for saving DD user info

--- DDLogg/Classes/DDBridge.m
+++ DDLogg/Classes/DDBridge.m
@@ -10,6 +10,11 @@
 @import DatadogObjc;
 
 @implementation DDBridge
+/**
+ * This function logs the input values to DataDog Logger.
+ * Also depends on the Feature flag if enabled
+ * This fun checks if logging is enabled from LD or not
+ */
 -(void)logToRemote:(NSString*)message with:(NSDictionary*)attributes{
     NSLog(@"logToRemote called");
     NSLog(@"message = %@",message);
@@ -22,6 +27,26 @@
     
     DDLogger *logger = [builder build];
     [logger info:message attributes:attributes];
+    
+}
+/**
+ * This sets the user info on DD platform to associate
+ * logs and RUM events to a specific customerID.
+ *
+ * Note : Sending Null resets the customer ID as this means user has logged out.
+ */
+-(void)setDataDogUserInfo:(BOOL)isSetUp email:(NSString*)email customerId:(int)customerId{
+    if (isSetUp) {
+        [DDDatadog setUserInfoWithId:[NSString stringWithFormat:@"%d", customerId] name:@"" email:email extraInfo:@{}];
+        DDRUMMonitor *monitor = [DDRUMMonitor new];
+        [monitor addAttributeForKey:@"usr.id" value:[NSString stringWithFormat:@"%d", customerId]];
+        [monitor addAttributeForKey:@"usr.email" value:email];
+        [DDGlobal setRum:monitor];
+    }else{
+        [DDDatadog setUserInfoWithId:@"" name:@"" email:@"" extraInfo:@{}];
+        [DDGlobal setRum:[DDRUMMonitor new]];
+    }
+    
 }
 
 @end
